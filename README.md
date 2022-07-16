8fd1def7a39f3ca7a0db54686c04989a0bc7b617
Frank <faqodfz@gmail.com>
2022-07-16 17:54:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto JournalAPP
+Proyecto de curso udemy React: de cero a experto de Fernando Herrera
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { JournalApp } from './journalApp';
+import { BrowserRouter } from 'react-router-dom';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
